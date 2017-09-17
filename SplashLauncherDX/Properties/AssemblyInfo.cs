48ed546f4d738651b1bfd8fb5990e8c2b6a5b25d
sh2mg136 <sh2mg136@gmail.com>
2017-09-17 11:51:53 +0300

    update project icon

--- SplashLauncherDX/Properties/AssemblyInfo.cs
+++ SplashLauncherDX/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Template")]
+[assembly: AssemblyTitle("RedExpress Update Launcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("RedExpress")]
 [assembly: AssemblyProduct("Template")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
